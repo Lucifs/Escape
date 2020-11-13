5affff66490f16b53b463034a0e3efb21351d4b2
Lucifs <lokis.2822@gmail.com>
2020-11-13 22:46:58 +0100

    Ordi quasi fini

---
+++ js/ordi-script.js
@@ -0,0 +1,29 @@
+// $('#un_autre_bouton').click(function(){
+//     $('#cache').show();
+//  });
+
+$('#fichier1').click(() => {
+        $('#texte1').show();
+    });
+$('#close1').click( function () {
+        $('#texte1').hide();
+    });
+$('#fichier2').click(() => {
+        $('#texte2').show();
+    });
+$('#close2').click( function () {
+        $('#texte2').hide();
+    });
+$('#fichier3').click(() => {
+        $('#texte3').show();
+    });
+$('#close3').click( function () {
+        $('#texte3').hide();
+    });
+
+$('#terminal').click(() => {
+        $('#terminal-command').show();
+    });
+$('#close-t').click( function () {
+        $('#terminal-command').hide();
+    });
