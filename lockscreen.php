00e953c861e8cce1d21581710d7f4358e41e03f7
louradoua <alou2000.78@gmail.com>
2020-11-19 15:47:19 +0100

    Modification de l'appel a bootstrap et jquery

--- lockscreen.php
+++ lockscreen.php
@@ -69,15 +69,15 @@
 		<map id="map1605720181048" name="map1605720181048">
 			<area shape="rect" coords="195,1312,531,1337" title="" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#auth">
 			<area shape="rect" coords="55,70,134,96" title="" alt="" href="links/operateur.html" target="_self">
-		</map>
-		
+		</map>	
 	</div>
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<!-- <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
 <script type="text/javascript" src="js/jquery.rwdImageMaps.js"></script>
 <script type="text/javascript" src="js/jquery.rwdImageMaps.min.js"></script>
 <script type="text/javascript">
