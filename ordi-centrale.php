2dca23e6e28e5bac507a4be96858bc048a72f539
Glouedec <lokis.2822@gmail.com>
2020-11-19 23:08:04 +0100

    Correction de l'erreur sur l'implémentation d'un scipte

--- ordi-centrale.php
+++ ordi-centrale.php
@@ -63,7 +63,7 @@
 
     </div>
 
-    <script type"text/javascript" src="js/scripte-banale.js" ></script>
+    <script type="text/javascript" src="js/scripte-banale.js" ></script>
     <script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
