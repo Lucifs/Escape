d7c7ed3f01e79943528be0aa035512ef62ac8ddf
Glouedec <lokis.2822@gmail.com>
2020-11-19 23:06:30 +0100

    Ajout d'un scripte banal

---
+++ js/scripte-banale.js
@@ -0,0 +1,5 @@
+console.log("Voter Casino Royalist', Voter le communisme !")
+
+console.log("Votez pour nous même si vous aimez pas le communisme.")
+
+console.log("Aller, j'arrête ici, la bise Royale.")
