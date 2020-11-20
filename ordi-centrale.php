230a9eb010d5f215ad8e83ad3e7a2335f8c7689a
louradoua <alou2000.78@gmail.com>
2020-11-20 11:04:52 +0100

    Test message ordi-centrale

--- ordi-centrale.php
+++ ordi-centrale.php
@@ -26,15 +26,17 @@
   <form id="fm1" action="login.php" method="post" autocomplete="off">
 
     <?php 
-    if ($_GET["error"]=="true"){
-    //   echo "<div id="msg" class="errors" style="
-    //                             background-color: rgb(255, 238, 221);
-    //                             border: 1px dotted #BB0000;
-    //                             color: #BB0000;
-    //                             padding-left: 100px;">
-    //           Mauvais identifiant / mot de passe.
-    //         </div>";
-      echo "<div>Error</div>"
+    if isset($_GET["error"]){
+      if ($_GET["error"]=="true"){
+      //   echo "<div id="msg" class="errors" style="
+      //                             background-color: rgb(255, 238, 221);
+      //                             border: 1px dotted #BB0000;
+      //                             color: #BB0000;
+      //                             padding-left: 100px;">
+      //           Mauvais identifiant / mot de passe.
+      //         </div>";
+        echo "<div>Error</div>"
+      }
     }
 
 
