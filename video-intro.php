a180a05b9aac8eeba636eec0b617d427aa54b191
louradoua <alou2000.78@gmail.com>
2020-11-20 19:43:29 +0100

    Create link between pages

---
+++ video-intro.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<?php
+	// header("Location : https://youtube.com/") //Lien vers la video !
+?>
+<html>
+<head>
+  <title>Escape</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">  
+<style type="text/css">
+	body {
+		margin:20px;
+	}
+
+
+</style>
+</head>
+
+<body>
+	<h1> Vidéo Youtube ! </h1>
+	<p>Bon, il reste à la tourner, mais quand c'est fait on décommente la ligne php du début et tout roule !</p>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/NVu9s-6lCl8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<p>Description :</p>
+	Choisissez votre rôle en concertation avec les membres de l'équipe
+	<ul>
+		<li><a href="">page1</a></li>
+		<li><a href="">page2</a></li>
+		<li><a href="">page3</a></li>
+		<li><a href="">page4</a></li>
+	</ul>
+
+
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.rwdImageMaps.js"></script>
+<script type="text/javascript" src="js/jquery.rwdImageMaps.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
