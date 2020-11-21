9755aa9f2f7c4e9f354c1c0a6f8d81e8f7385aa7
louradoua <alou2000.78@gmail.com>
2020-11-21 16:05:50 +0100

    Redone liege

--- liege.php
+++ liege.php
@@ -2,11 +2,11 @@
 <html>
 
 <head>
-	<title>Escape</title>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/style-liege.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <title>Escape</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style-liege.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 
 <body>
