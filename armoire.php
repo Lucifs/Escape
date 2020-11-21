3d5c3257ceeabde81407255072a79a345065fd1a
louradoua <alou2000.78@gmail.com>
2020-11-21 14:32:10 +0100

    Created armoire

--- armoire.php
+++ armoire.php
@@ -5,11 +5,30 @@
   <title>Escape</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">  <!--<link rel="stylesheet" type="text/css" href="css/style.css">-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> 
+<link rel="stylesheet" type="text/css" href="css/style-armoire.css">
 </head>
 
 <body>
-	<h1>Ceci est une armoire</h1>
+	<div class="fullHeight text-center page">
+		<img class="fullHeight" src="images/armoire.png" usemap="#map1605569696870">
+
+		<map id="map1605569696870" name="map1605569696870">
+			<area shape="rect" coords="87,154,190,256" title="calendar" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#calendar">
+			<area shape="rect" coords="235,155,335,257" title="photos" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#photos">
+			<area shape="rect" coords="379,155,482,256" title="camera" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#camera">
+			<area shape="rect" coords="525,154,628,255" title="notes" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#notes">
+			<area shape="rect" coords="86,313,189,416" title="maps" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#maps">
+			<area shape="rect" coords="232,315,334,416" title="clock" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#clock">
+			<area shape="rect" coords="377,313,484,417" title="files" alt="" href="file.pdf" download="IMPORTANTTRESIMPORTANT.pdf">
+			<area shape="rect" coords="72,61,104,89" title="easter1" alt="easter1" href="#scrapModal" target="_self"data-toggle="modal" data-target="#easter1">
+			<area shape="rect" coords="87,1255,191,1356" title="phone" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#phone">
+			<area shape="rect" coords="233,1255,337,1357" title="messages" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#messages">
+			<area shape="rect" coords="379,1254,483,1357" title="meteo" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#meteo">
+			<area shape="rect" coords="528,1253,628,1357" title="settings" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#settings">
+			<area shape="rect" coords="577,65,599,87" title="easter2" alt="easter2" href="#scrapModal" target="_self"data-toggle="modal" data-target="#esater2">
+		</map>
+	</div>
 
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/jquery.rwdImageMaps.js"></script>
