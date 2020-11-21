762488cadc3def3a4d47225ab3d34bbe5509f114
louradoua <alou2000.78@gmail.com>
2020-11-21 16:11:57 +0100

    Liege responsive

--- liege.php
+++ liege.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
+
+
 <html>
 
 <head>
   <title>Escape</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="css/style-liege.css">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style-liege.css">
 </head>
 
 <body>
@@ -156,14 +158,11 @@
 </div>
 
 
-
-
-<div class="fullHeight text-center page">
+  <div class="fullHeight text-center page">
     <img class="fullHeight" src="images/liege.png" usemap="#map1605569696870">
 
-
-
-</div>
+    
+  </div>
 
 
 
@@ -174,5 +173,11 @@
 <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+<script type="text/javascript">
+  $(document).ready(function(e) {
+      $('img[usemap]').rwdImageMaps();
+  });
+</script>
+
+
 </body>
-</html>
