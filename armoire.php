c50e5edb88b8700ee8cfdc38221f217827dff1ad
louradoua <alou2000.78@gmail.com>
2020-11-21 16:04:17 +0100

    Udatad style armoire

--- armoire.php
+++ armoire.php
@@ -10,31 +10,107 @@
 </head>
 
 <body>
-	<div class="fullHeight text-center page">
-		<img class="fullHeight" src="images/armoire.png" usemap="#map1605569696870">
 
-		<map id="map1605569696870" name="map1605569696870">
-			<area shape="rect" coords="87,154,190,256" title="calendar" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#calendar">
-			<area shape="rect" coords="235,155,335,257" title="photos" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#photos">
-			<area shape="rect" coords="379,155,482,256" title="camera" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#camera">
-			<area shape="rect" coords="525,154,628,255" title="notes" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#notes">
-			<area shape="rect" coords="86,313,189,416" title="maps" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#maps">
-			<area shape="rect" coords="232,315,334,416" title="clock" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#clock">
-			<area shape="rect" coords="377,313,484,417" title="files" alt="" href="file.pdf" download="IMPORTANTTRESIMPORTANT.pdf">
-			<area shape="rect" coords="72,61,104,89" title="easter1" alt="easter1" href="#scrapModal" target="_self"data-toggle="modal" data-target="#easter1">
-			<area shape="rect" coords="87,1255,191,1356" title="phone" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#phone">
-			<area shape="rect" coords="233,1255,337,1357" title="messages" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#messages">
-			<area shape="rect" coords="379,1254,483,1357" title="meteo" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#meteo">
-			<area shape="rect" coords="528,1253,628,1357" title="settings" alt="" href="#scrapModal" target="_self"data-toggle="modal" data-target="#settings">
-			<area shape="rect" coords="577,65,599,87" title="easter2" alt="easter2" href="#scrapModal" target="_self"data-toggle="modal" data-target="#esater2">
-		</map>
+<div class="modal fade bd-example-modal-lg" data-animate-in='animate__zoomInUp' data-animate-out='animate__flipOutY' id="locker1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content modal-content-dark">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Locker</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Ceci est le corps du locker 1
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade bd-example-modal-lg" data-animate-in='animate__zoomInUp' data-animate-out='animate__flipOutY' id="locker2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content modal-content-dark">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Locker</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Ceci est le corps du locker 2
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade bd-example-modal-lg" data-animate-in='animate__zoomInUp' data-animate-out='animate__flipOutY' id="locker3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content modal-content-dark">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Locker</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Ceci est le corps du locker 3
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade bd-example-modal-lg" data-animate-in='animate__zoomInUp' data-animate-out='animate__flipOutY' id="largelocker" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content modal-content-dark">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Locker</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Ceci est le corps du locker large
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+	<div class="fullHeight text-center page">
+		<img class="fullHeight" src="images/armoire.png" usemap="#map1605965744173">
+		
+			<map id="map1605965744173" name="map1605965744173">
+				<area shape="rect" coords="1160.9999752128906,494.99146080859373,1305.0113392128906,666.0028248085937" title="" alt="" href="#scrapModal" target="_self" data-toggle="modal" data-target="#locker1">
+				<area shape="rect" coords="1161.0000057304687,668.997137078125,1305.0113697304687,847.0085010781249" title="" alt="" href="#scrapModal" target="_self" data-toggle="modal" data-target="#locker2">
+				<area shape="rect" coords="1156.97917,846.9792005175782,1306.3125100000002,1197.3125405175783" title="" alt="" href="#scrapModal" target="_self" data-toggle="modal" data-target="#locker3">
+				<area shape="rect" coords="1063.0000440234376,820.9999829882813,1125.3333840234375,871.3333229882813" title="" alt="" href="#scrapModal" target="_self" data-toggle="modal" data-target="#largelocker">
+			</map>
 	</div>
 
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/jquery.rwdImageMaps.js"></script>
 <script type="text/javascript" src="js/jquery.rwdImageMaps.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
-<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+<script type="text/javascript">
+	$(document).ready(function(e) {
+    	$('img[usemap]').rwdImageMaps();
+	});
+</script>
 </body>
